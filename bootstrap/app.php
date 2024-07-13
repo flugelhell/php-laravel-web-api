62a24ed0f06c969a3990f0039e0f8481de7d06c4
flugelhell <yayat.suyatna@lygapparel.co.id>
2024-07-14 03:01:04 +0700

    first commit

---
+++ bootstrap/app.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Middleware\AuthApiMiddleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Add Middleware API
+        $middleware->append(AuthApiMiddleware::class);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        // Add Exception For API
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage(),
+                ], 401);
+            }
+        });
+    })->create();
